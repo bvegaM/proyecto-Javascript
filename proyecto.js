015d48862c1842f53f318dcad040d16cf4e5e453
bvegaM <vegabryam40@gmail.com>
2020-11-02 16:50:03 -0500

    iluminando las casillas

--- proyecto.js
+++ proyecto.js
@@ -1,13 +1,14 @@
 const celeste = document.getElementById('celeste');
 const rojo = document.getElementById('rojo');
 const naranja = document.getElementById('naranja');
-const amarillo = document.getElementById('amararillo');
+const amarillo = document.getElementById('amarillo');
 const btnEmpezar = document.getElementById('btnEmpezar');
 
 class Juego {
     constructor() {
         this.inicializar();
         this.generarSecuencia();
+        this.siguienteNivel();
     }
 
     inicializar() {
@@ -23,6 +24,39 @@ class Juego {
 
     generarSecuencia(){
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4));
+    }
+
+    siguienteNivel(){
+        this.iluminarSecuencia();
+    }
+
+    transformarNumeroAColor(num){
+        switch(num){
+            case 0:
+                return `celeste`
+            case 1:
+                return `rojo`;
+            case 2:
+                return `naranja`;
+            case 3:
+                return `amarillo`;
+        }
+    }
+
+    iluminarSecuencia(){
+        for(var i=0; i < this.nivel;i++){
+            let color = this.transformarNumeroAColor(this.secuencia[i]);
+            setTimeout(()=>this.iluminarColor(color),1000*i);
+        }
+    }
+
+    iluminarColor(color){
+        this.colores[color].classList.add('light');
+        setTimeout(()=> this.apagarColor(color),350);
+    }
+
+    apagarColor(color){
+        this.colores[color].classList.remove('light');
     }
 }
 function empezarJuego() {
